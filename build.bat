7cde7a48d25d82833fd5546082a9a39443c05b4a
Engin Cilasun <ecilasun@me.com>
2025-12-14 13:57:10 -0800

    Added prebuilt binaries

--- build.bat
+++ build.bat
@@ -1,3 +1,10 @@
 @echo off
 set PATH=C:\msys64\mingw64\bin;C:\msys64\usr\bin;%PATH%
 ninja -C build
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+if not exist bin mkdir bin
+echo Copying files to bin...
+copy /Y build\qemu-system-arm.exe bin\
+if exist zImage_h copy /Y zImage_h bin\
+if exist system_h.dtb copy /Y system_h.dtb bin\
