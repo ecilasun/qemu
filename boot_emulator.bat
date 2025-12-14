72c8119f80abc245fdb18532b28339159c4fd5c0
Engin Cilasun <ecilasun@me.com>
2025-12-14 00:59:12 -0800

    Emulator boot script

---
+++ boot_emulator.bat
@@ -0,0 +1,4 @@
+@echo off
+set PATH=C:\msys64\mingw64\bin;C:\msys64\usr\bin;%PATH%
+build\qemu-system-arm.exe -M sandpiper -m 1024 -serial stdio -drive file=extracted_sdcard.img,if=sd,format=raw -kernel zImage_h -dtb system_h.dtb -append "console=ttyPS0,115200 root=/dev/mmcblk0p2 rw rootwait earlyprintk"
+pause
