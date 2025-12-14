6f893d30da27b461329f448e09f1935a258fcc63
Engin Cilasun <ecilasun@me.com>
2025-12-14 04:35:49 -0800

    Added terminal.bat to connect to emulator

--- boot_emulator.bat
+++ boot_emulator.bat
@@ -1,4 +1,4 @@
 @echo off
 set PATH=C:\msys64\mingw64\bin;C:\msys64\usr\bin;%PATH%
-build\qemu-system-arm.exe -M sandpiper -m 1024 -serial tcp::5555,server,nowait -drive file=extracted_sdcard.img,if=sd,format=raw -kernel zImage_h -dtb system_h.dtb -append "console=ttyPS0,115200 root=/dev/mmcblk0p2 rw rootwait earlyprintk"
+build\qemu-system-arm.exe -M sandpiper -m 1024 -serial tcp::5555,server,nowait -drive file=extracted_sdcard.img,if=sd,format=raw -kernel zImage_h -dtb system_h.dtb -append "console=ttyPS0,115200 root=/dev/mmcblk0p2 rw rootwait earlyprintk" -net nic -net user,hostfwd=tcp::2222-:22
 pause
