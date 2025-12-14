3b72127a44d33e85c786ced703b4e580507b635a
Engin Cilasun <ecilasun@me.com>
2025-12-14 13:59:26 -0800

    Reduced emulated system memory to match hardware (512Mbytes of RAM)

--- boot_emulator.bat
+++ boot_emulator.bat
@@ -1,6 +1,6 @@
 @echo off
 set PATH=C:\msys64\mingw64\bin;C:\msys64\usr\bin;%PATH%
 cd bin
-qemu-system-arm.exe -M sandpiper -m 1024 -serial tcp::5555,server,nowait -drive file=extracted_sdcard.img,if=sd,format=raw -kernel zImage_h -dtb system_h.dtb -append "console=ttyPS0,115200 root=/dev/mmcblk0p2 rw rootwait earlyprintk" -net nic -net user,hostfwd=tcp::2222-:22
+qemu-system-arm.exe -M sandpiper -m 512 -serial tcp::5555,server,nowait -drive file=extracted_sdcard.img,if=sd,format=raw -kernel zImage_h -dtb system_h.dtb -append "console=ttyPS0,115200 root=/dev/mmcblk0p2 rw rootwait earlyprintk" -net nic -net user,hostfwd=tcp::2222-:22
 cd ..
 pause
