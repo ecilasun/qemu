3589a09ef56b10d5a46f435c6cc637cd42ada714
Engin Cilasun <ecilasun@me.com>
2025-12-24 21:28:31 -0800

    Matching hardware boot command line

--- bin/boot_emulator.sh
+++ bin/boot_emulator.sh
@@ -1,1 +1,1 @@
-./qemu-system-arm -M sandpiper -m 1024 -serial tcp::5555,server,nowait -drive file=sdcard.qcow2,if=sd,format=qcow2 -kernel zImage_h -dtb system_h.dtb -append "console=ttyPS0,115200 root=/dev/mmcblk0p2 rw rootwait earlyprintk" -net nic -net user,hostfwd=tcp::2222-:22
+./qemu-system-arm -M sandpiper -m 1024 -serial tcp::5555,server,nowait -drive file=sdcard.qcow2,if=sd,format=qcow2 -kernel zImage_h -dtb system_h.dtb -append "console=ttyPS0,115200 console=tty1 root=/dev/mmcblk0p2 rw rootwait earlyprintk" -net nic -net user,hostfwd=tcp::2222-:22
