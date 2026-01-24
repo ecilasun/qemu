5b201e0d5067fc2499dcf8ce4ff6d3deb455f8d7
Engin Cilasun <ecilasun@me.com>
2026-01-24 15:13:21 -0800

    added reminder

--- bin/boot_emulator.bat
+++ bin/boot_emulator.bat
@@ -1,3 +1,4 @@
+echo Please make sure that C:\msys64\mingw64\bin and C:\msys64\usr\bin are in your PATH environment variable.
 @echo off
 set PATH=C:\msys64\mingw64\bin;C:\msys64\usr\bin;%PATH%
 qemu-system-arm.exe -M sandpiper -m 1024 -serial tcp::5555,server,nowait -drive file=sdcard.qcow2,if=sd,format=qcow2 -kernel zImage_h -dtb system_h.dtb -append "console=ttyPS0,115200 console=tty1 root=/dev/mmcblk0p2 rw rootwait earlyprintk" -net nic -net user,hostfwd=tcp::2222-:22
