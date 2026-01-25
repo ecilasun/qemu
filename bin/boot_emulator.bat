fb64e04d9f294c881d941dea759a168bf496e1d2
Engin Cilasun <ecilasun@me.com>
2026-01-24 19:46:55 -0800

    startup fix for vnc connection on windows

--- bin/boot_emulator.bat
+++ bin/boot_emulator.bat
@@ -1,5 +1,3 @@
-echo Please make sure that C:\msys64\mingw64\bin and C:\msys64\usr\bin are in your PATH environment variable.
 @echo off
-set PATH=C:\msys64\mingw64\bin;C:\msys64\usr\bin;%PATH%
-qemu-system-arm.exe -M sandpiper -m 1024 -serial tcp::5555,server,nowait -drive file=sdcard.qcow2,if=sd,format=qcow2 -kernel zImage_h -dtb system_h.dtb -append "console=ttyPS0,115200 console=tty1 root=/dev/mmcblk0p2 rw rootwait earlyprintk" -net nic -net user,hostfwd=tcp::2222-:22
+qemu-system-arm.exe -L ..\pc-bios -M sandpiper -m 512M -serial tcp::5555,server,nowait -drive file=sdcard.qcow2,if=sd,format=qcow2 -kernel zImage_h -dtb system_h.dtb -append "console=ttyPS0,115200 root=/dev/mmcblk0p2 earlyprintk" -net nic -net user,hostfwd=tcp::2222-:22 
 pause
