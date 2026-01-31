2b103c7a192cafeb8aec8bf424f2e45f9a0201e8
Engin Cilasun <ecilasun@me.com>
2026-01-31 15:53:34 -0800

    fixed linux build

--- build.sh
+++ build.sh
@@ -28,12 +28,25 @@ if [ "$(uname -s)" = "Darwin" ]; then
 			install_name_tool -add_rpath "@executable_path/" "$target"
 		fi
 	fi
+else
+	# On Linux, set RPATH to look in the same directory as the executable
+	target="bin/${system_bin}"
+	if command -v patchelf >/dev/null 2>&1; then
+		echo "Setting RPATH for ${system_bin}..."
+		patchelf --set-rpath '$ORIGIN' "$target"
+	else
+		echo "Warning: patchelf not found. Installing it is recommended for proper library loading."
+		echo "You can install it with: sudo apt-get install patchelf"
+	fi
 fi
 
-if [ -f zImage_h ]; then
-    cp zImage_h bin/
-fi
 
 if [ -f system_h.dtb ]; then
     cp system_h.dtb bin/
+fi
+
+# Copy libslirp library
+if [ -f build/subprojects/slirp/libslirp.so.0.4.0 ]; then
+    echo "Copying libslirp to bin..."
+    cp -P build/subprojects/slirp/libslirp.so* bin/
 fi
